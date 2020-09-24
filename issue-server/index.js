282f3f04e063c7f9c90e3325510ea8064dd7332b
ion rosgrim <jrunin@gmail.com>
2020-09-25 00:16:31 +0200

    refactor

--- issue-server/index.js
+++ issue-server/index.js
@@ -3,17 +3,15 @@ const express = require('express');
 const path = require('path');
 const { setupDb } = require('./db/dbHelpers');
 const { Route } = require('./routes/routes');
-const setup = require('./db/dbHelpers').setup;
-const checkAuthorisation= require('./middlewares/middlewares').checkAuthorisation;
+const checkAuthorisation = require('./middlewares/middlewares').checkAuthorisation;
 const app = express();
-const port = 3002;
+const port = process.env.ISSUE_SERVER_PORT;
 const route = new Route();
-const baseUrl="/issue-tracker";
+const baseUrl = process.env.BASE_URL;
+app.listen(port, () => console.log('Issue server running on port ' + port));
+
 setupDb();
 
 app.use(baseUrl, express.static(path.join(__dirname, 'www')));
 
-
 app.get(`${baseUrl}/all-issues`, checkAuthorisation(), route.getOngoingIssues());
-
-app.listen(port, () => console.log('Issue server running on port ' + port));
