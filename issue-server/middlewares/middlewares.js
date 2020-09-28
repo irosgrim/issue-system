bc805f198be6ed78eb02e2f195d5191764430114
ioan rosgrim <ioan.rosgrim@sembo.se>
2020-09-28 20:24:59 +0200

    check aouthorization

--- issue-server/middlewares/middlewares.js
+++ issue-server/middlewares/middlewares.js
@@ -1,9 +1,23 @@
-function checkAuthorisation() {
+function checkAuthorisation(req, res, next) {
     return (req, res, next) => {
-        console.log('authorised, for now ');
-        next();
+        console.log('authorised, for now ');return async (req, res, next) => {
+            const token = req.cookies.token || null;
+            if(!token) {
+                req.authorisedUser = null;
+                next();
+            } else {
+                try {
+                    const tokenInfo = await jwt.verify(token, jwtSecret);
+                    req.authorisedUser = tokenInfo.username;
+                    next();
+                } catch(err) {
+                    console.log('Invalid token');
+                }
+            }
+        }
     }
 }
+
 module.exports = {
     checkAuthorisation
 }
